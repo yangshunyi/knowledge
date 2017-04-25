59bfbb69d9b63ccabeddbbe45b893a45651dc03c
yangshunyi <1241485371@qq.com>
2017-04-25 18:20:30 +0800

    登录
    首页想要登录

---
+++ js/main.js
@@ -0,0 +1,24 @@
+require(["jquery/jquery.1.8.3"],function(){
+	$.getJSON("json/user.json",function(data){
+		
+		
+	})
+	
+	var userVail = (function(){
+		
+		var bind = function(){
+			$(".user-btn").on("click",function(){
+				
+			})
+		};
+		var vail = function(data){
+			var name= $("input[name='username']").val();
+		}
+		
+		
+		
+		
+	})()
+	
+	
+})
